3e629874fdfc0f166eef330decc9d789ddcc92f5
Zachary Ellison <zach.r.ellison@gmail.com>
2026-01-23 18:49:57 -0500

    some gdscript

---
+++ flappy/scenes/player.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var speed = 5 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var theta = 0
+	if Input.is_action_pressed("press"):
+		theta += speed 
